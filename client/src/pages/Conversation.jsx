72d9f476fa1d608b03dad198e24239191f4966f7
Euro Abo <abaoeuro2002@gmail.com>
2024-05-17 04:54:34 +0800

    button changed to blue

--- client/src/pages/Conversation.jsx
+++ client/src/pages/Conversation.jsx
@@ -35,7 +35,7 @@ function Conversation() {
             placeholder="Write a mesage"
             className="flex-1 px-4 py-2 rounded-full outline outline-1 focus-within:outline-sky-500 outline-slate-300"
           />
-          <div className="flex items-center justify-center w-10 text-lg text-white bg-red-500 rounded-full aspect-square">
+          <div className="flex items-center justify-center w-10 text-lg text-white rounded-full bg-sky-500 aspect-square">
             <BiSolidSend />
           </div>
         </div>
