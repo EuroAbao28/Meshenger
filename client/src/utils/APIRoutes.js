59cb8d23db0dee6fd2d51f906f4c635174367779
Euro Abo <abaoeuro2002@gmail.com>
2024-08-15 20:06:08 +0800

    backend live url

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:5000";
+export const host = "https://meshenger.onrender.com";
 export const offlinClientURL = "http://localhost:5173";
 
 export const userRoute = `${host}/api/user`;
